36fe683fbf6e06219a6f07e243d039c68af9df38
Uyenochris <102437571+Uyenochris@users.noreply.github.com>
2023-09-13 15:00:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function calc() {
+    let principle=document.getElementById("principle").Value
+    let rate=document.getElementById("interest-rate").Value
+    let time=document.getElementById("time").Value
+  //calculations go here
+}
